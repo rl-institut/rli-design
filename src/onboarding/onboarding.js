5704fe706f9b7194b3200400ac13e98cdc6ce3fe
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-02-22 14:19:29 +0100

    Add local JS for onboarding

---
+++ src/onboarding/onboarding.js
@@ -0,0 +1,4 @@
+// Your page specific JavaScript here
+
+// Majd, Please delete this console.log() function call and make this file your own!
+console.log("Page specfic code in src/onboarding/onboarding.js initialized!");
