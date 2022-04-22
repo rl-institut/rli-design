b5293d8520d9a39528fcfe5759ede0a44e8abc8a
bmlancien <bryan.lancien@rl-institut.de>
2022-04-22 10:25:36 +0200

    move db chart js code to own js file

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,5 +1,45 @@
-// Your page specific JavaScript here
+const dbChart = echarts.init(document.getElementById('mainChart'));
 
-// This file can be named anything
+const option = {
+  title: {
+    text: 'Budget in Tsd. €'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  dataset: {
+    source: [
+      ['Kategorie', 'IST', 'Forecast', 'Abbau Überschüsse, Puffer und Risikobudget in Tsd. €'],
+      ['Personal', -12, -25, -65],
+      ['Eigenmittel', 72, 95, -65],
+    ]
+  },
+  xAxis: { type: 'category' },
+  yAxis: {},
+  series: [{ type: 'bar' }, { type: 'bar' }, {'type': 'line'}],
+  color: [
+    '#002C50',
+    '#FFE8B3'
+  ],
+  label: {
+    normal: {
+      position: 'top',
+      distance: 10,
+      show: true,
+      formatter: ['Label Text'].join('\n')
+    }
+  }
+};
 
-console.log("Test");
+function resizeChart() {
+    setTimeout(function () {
+      dbChart.resize();
+    }, 200);
+  };
+
+dbChart.setOption(option);
+
+window.addEventListener('resize', resizeChart);
