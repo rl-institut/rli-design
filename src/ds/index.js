e26e991ea13cc8675a79ce77619edfb6906545e5
bmlancien <bryan.lancien@rl-institut.de>
2022-07-06 17:44:07 +0200

    add colors to design tokens table

--- src/ds/index.js
+++ src/ds/index.js
@@ -42,11 +42,20 @@ const designTokensTable = document.getElementById("js-dtTable");
 let designTokensTableContent = '';
 for (let i = 0; i < designTokens.length; i++) {
   designTokensTableContent += `<tr>
-    <td class="dt-table__token"><span>${designTokens[i]}</span></td>
-    <td class="dt-table__roles">${dtRoles[i]}</td>
-    <td class="dt-table__variables"><code>${dtVariables[i]}</code></td>
-    <td class="dt-table__utilities"><code>${dtUtilities[i]}</code></td>
-    </tr>`;
+  <td class="dt-table__token"><span>${designTokens[i]}</span></td>
+  <td class="dt-table__roles">${dtRoles[i]}</td>
+  <td class="dt-table__variables"><code>${dtVariables[i]}</code></td>
+  <td class="dt-table__utilities"><code>${dtUtilities[i]}</code></td>`
+
+  if (tokenCategories[i] === "colors" && tokenUsages[i] === "background") {
+    designTokensTableContent += `<td class="dt-table__display dt-table__display--bg"><span class="${dtUtilities[i].slice(1)}"></span></td>`;
+  } else if (tokenCategories[i] === "colors" && tokenUsages[i] === "text") {
+    designTokensTableContent += `<td class="dt-table__display dt-table__display--text"><span class="${dtUtilities[i].slice(1)}">Text color</span></td>`;
+  } else {
+    designTokensTableContent += `<td></td>`
+  }
+
+  designTokensTableContent += `</tr>`;
   designTokensTable.innerHTML = designTokensTableContent;
 }
 
