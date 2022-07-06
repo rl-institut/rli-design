12c29defbbe2636ba14935dd1d71a4992bdecbc7
bmlancien <bryan.lancien@rl-institut.de>
2022-07-06 16:11:35 +0200

    populate design token table

--- src/ds/index.js
+++ src/ds/index.js
@@ -1,5 +1,15 @@
 import tokens from "./dt.json";
 
+// Variables
+const designTokens = tokens.map((token) => `${token.name}.${token.value.name}.${token.value.value.name}.${token.value.value.value.name}`);
+const dtRoles = tokens.map((token) => token.value.value.value.value.description);
+const dtVariables = tokens.map((token) => token.value.value.value.value.variable);
+const dtUtilities = tokens.map((token) => token.value.value.value.value.utility);
+
+const tokenCategories = tokens.map((token) => token.name);
+const tokenUsages = tokens.map((token) => token.value.name);
+const tokenComponents = tokens.map((token) => token.value.value.name);
+
 const categories = [
   "borderRadius",
   "colors",
@@ -27,10 +37,15 @@ const categoryDescription = "category";
 const usageDescription = "usage";
 const componentDescription = "component";
 
-const tokenCategories = tokens.map((token) => token.name);
-const tokenUsages = tokens.map((token) => token.value.name);
-const tokenComponents = tokens.map((token) => token.value.value.name);
+// Populate table
+const designTokensTable = document.getElementById("js-dtTable");
+let designTokensTableContent = '';
+for (let i = 0; i < designTokens.length; i++) {
+  designTokensTableContent += `<tr><td>${designTokens[i]}</td><td>${dtRoles[i]}</td><td>${dtVariables[i]}</td><td>${dtUtilities[i]}</td></tr>`;
+  designTokensTable.innerHTML = designTokensTableContent;
+}
 
+// Test names
 function testNames(token, name, description) {
   const namesAreValid = token.every(e=> name.includes(e));
   if (!namesAreValid) {
