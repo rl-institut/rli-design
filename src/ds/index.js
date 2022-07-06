a03e3cff82c3c64c04308be52778edb1ff364dda
bmlancien <bryan.lancien@rl-institut.de>
2022-07-06 12:02:58 +0200

    add test to check if dt category names are valid

--- src/ds/index.js
+++ src/ds/index.js
@@ -1,3 +1,19 @@
 import tokens from "./dt.json";
 
-tokens.map((token) => console.log(token.value.value.value));
+const categories = [
+  "borderRadius",
+  "colors",
+  "spacing",
+  "typography"
+];
+
+const tokensCategories = tokens.map((token) => token.name);
+
+// Test if all categories are correct
+const tokensCategoriesAreValid = tokensCategories.every(value=> categories.includes(value));
+
+if (!tokensCategoriesAreValid) {
+  console.error("Some token category names aren't correct");
+  console.log(tokensCategories);
+}
+
