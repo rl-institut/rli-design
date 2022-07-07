c73235d38101d56b8e57d6f7fc91e798cf679949
bmlancien <bryan.lancien@rl-institut.de>
2022-07-07 18:28:27 +0200

    change design token structure

--- src/ds/index.js
+++ src/ds/index.js
@@ -1,14 +1,32 @@
-import tokens from "./dt.json";
+import tokens from "./temp.json";
 
 // Variables
-const designTokens = tokens.map((token) => `${token.name}.${token.value.name}.${token.value.value.name}.${token.value.value.value.name}`);
-const dtRoles = tokens.map((token) => token.value.value.value.value.description);
-const dtVariables = tokens.map((token) => token.value.value.value.value.variable);
-const dtUtilities = tokens.map((token) => token.value.value.value.value.utility);
-
+//const designTokens = tokens.map((token) => `${token.name}.${token.value.name}.${token.value.value.name}.${token.value.value.value.name}.${token.value.value.value.value.name}`);
 const tokenCategories = tokens.map((token) => token.name);
 const tokenUsages = tokens.map((token) => token.value.name);
 const tokenComponents = tokens.map((token) => token.value.value.name);
+const tokenModifiers = tokens.map((token) => token.value.value.value.name);
+const tokenStates = tokens.map((token) => token.value.value.value.value.name);
+const dtRoles = tokens.map((token) => token.value.value.value.value.value.description);
+const dtVariables = tokens.map((token) => token.value.value.value.value.value.variable);
+const dtUtilities = tokens.map((token) => token.value.value.value.value.value.utility);
+
+let designTokens = [];
+
+function createDesignTokens() {
+  let dt = "";
+
+  for (let i = 0; i < tokenCategories.length; i++) {
+    if (typeof tokenModifiers[i] === "undefined" && typeof tokenStates[i] === "undefined") {
+      designTokens.push(`${tokenCategories[i]}.${tokenUsages[i]}.${tokenComponents[i]}`);
+    } else if (typeof tokenStates[i] === "undefined") {
+      designTokens.push(`${tokenCategories[i]}.${tokenUsages[i]}.${tokenComponents[i]}.${tokenModifiers[i]}`);
+    } else {
+      designTokens.push(`${tokenCategories[i]}.${tokenUsages[i]}.${tokenComponents[i]}.${tokenModifiers[i]}.${tokenStates[i]}`);
+    }
+  }
+  return designTokens;
+};
 
 const categories = [
   "borderRadius",
@@ -39,10 +57,12 @@ const componentDescription = "component";
 
 // Populate table
 const designTokensTable = document.getElementById("js-dtTable");
-let designTokensTableContent = '';
+const dt = createDesignTokens();
+let designTokensTableContent = "";
+
 for (let i = 0; i < designTokens.length; i++) {
   designTokensTableContent += `<tr>
-  <td class="dt-table__token"><span>${designTokens[i]}</span></td>
+  <td class="dt-table__token"><span>${dt[i]}</span></td>
   <td class="dt-table__roles">${dtRoles[i]}</td>
   <td class="dt-table__variables"><code>${dtVariables[i]}</code></td>
   <td class="dt-table__utilities"><code>${dtUtilities[i]}</code></td>`
