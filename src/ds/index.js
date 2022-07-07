bf2a521db6bb793b63f8d3924a55643fb665d1a9
bmlancien <bryan.lancien@rl-institut.de>
2022-07-07 13:32:37 +0200

    update token names

--- src/ds/index.js
+++ src/ds/index.js
@@ -18,19 +18,19 @@ const categories = [
 ];
 
 const usages = [
-  "background",
-  "fontSize",
-  "fontWeight",
+  "bg",
+  "fs",
+  "fw",
   "nav",
   "padding",
-  "text"
+  "txt"
 ];
 
 const components = [
   "body",
-  "button",
-  "dashboard",
-  "sidebarAnchor"
+  "btn",
+  "db",
+  "sideLink"
 ]
 
 const categoryDescription = "category";
@@ -47,9 +47,9 @@ for (let i = 0; i < designTokens.length; i++) {
   <td class="dt-table__variables"><code>${dtVariables[i]}</code></td>
   <td class="dt-table__utilities"><code>${dtUtilities[i]}</code></td>`
 
-  if (tokenCategories[i] === "color" && tokenUsages[i] === "background") {
+  if (tokenCategories[i] === "color" && tokenUsages[i] === "bg") {
     designTokensTableContent += `<td class="dt-table__display dt-table__display--bg"><span class="${dtUtilities[i].slice(1)}"></span></td>`;
-  } else if (tokenCategories[i] === "color" && tokenUsages[i] === "text") {
+  } else if (tokenCategories[i] === "color" && tokenUsages[i] === "txt") {
     designTokensTableContent += `<td class="dt-table__display dt-table__display--text"><span class="${dtUtilities[i].slice(1)}">Text color</span></td>`;
   } else {
     designTokensTableContent += `<td></td>`
