d9f018f8a08a9f31992de51457bd9ac7e68cb9f0
bmlancien <bryan.lancien@rl-institut.de>
2022-07-07 13:18:10 +0200

    change colors token to color

--- src/ds/index.js
+++ src/ds/index.js
@@ -12,7 +12,7 @@ const tokenComponents = tokens.map((token) => token.value.value.name);
 
 const categories = [
   "borderRadius",
-  "colors",
+  "color",
   "spacing",
   "typography"
 ];
@@ -47,9 +47,9 @@ for (let i = 0; i < designTokens.length; i++) {
   <td class="dt-table__variables"><code>${dtVariables[i]}</code></td>
   <td class="dt-table__utilities"><code>${dtUtilities[i]}</code></td>`
 
-  if (tokenCategories[i] === "colors" && tokenUsages[i] === "background") {
+  if (tokenCategories[i] === "color" && tokenUsages[i] === "background") {
     designTokensTableContent += `<td class="dt-table__display dt-table__display--bg"><span class="${dtUtilities[i].slice(1)}"></span></td>`;
-  } else if (tokenCategories[i] === "colors" && tokenUsages[i] === "text") {
+  } else if (tokenCategories[i] === "color" && tokenUsages[i] === "text") {
     designTokensTableContent += `<td class="dt-table__display dt-table__display--text"><span class="${dtUtilities[i].slice(1)}">Text color</span></td>`;
   } else {
     designTokensTableContent += `<td></td>`
