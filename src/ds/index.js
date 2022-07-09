586bbaab34669fcde7501801f768539180572a20
bmlancien <bryan.lancien@rl-institut.de>
2022-07-09 16:45:58 +0200

    add table dts

--- src/ds/index.js
+++ src/ds/index.js
@@ -24,6 +24,7 @@ const categories = [
 ];
 
 const usages = [
+  "align",
   "bg",
   "br",
   "border",
@@ -72,9 +73,9 @@ for (let i = 0; i < designTokens.length; i++) {
   <td class="dt-table__variables"><code>${dtVariables[i]}</code></td>
   <td class="dt-table__utilities"><code>${dtUtilities[i]}</code></td>`
 
-  if (tokenCategories[i] === "color" && tokenUsages[i] === "bg") {
+  if (tokenCategories[i] === "clr" && tokenUsages[i] === "bg") {
     designTokensTableContent += `<td class="dt-table__display dt-table__display--bg"><span class="${dtUtilities[i].slice(1)}"></span></td>`;
-  } else if (tokenCategories[i] === "color" && tokenUsages[i] === "txt") {
+  } else if (tokenCategories[i] === "clr" && tokenUsages[i] === "txt") {
     designTokensTableContent += `<td class="dt-table__display dt-table__display--text"><span class="${dtUtilities[i].slice(1)}">Text color</span></td>`;
   } else {
     designTokensTableContent += `<td></td>`
