43d943bf50cdc8be8f066d77dcbc6ce63f691d81
bmlancien <bryan.lancien@rl-institut.de>
2022-07-27 12:02:10 +0200

    add html source code as snippet

--- src/ds/table/index.js
+++ src/ds/table/index.js
@@ -1,7 +1,11 @@
 const dashboardTable = document.getElementById('jsDashboardTable');
+const dashboardTableText = dashboardTable.innerHTML;
 const dashboardTableChild = dashboardTable.firstElementChild;
 const selectClass = document.getElementById('js-select-class');
+let tableCode = document.getElementById('js-table-code');
 let selectedOption = selectClass.value;
+
+tableCode.append(dashboardTableText);
 
 selectClass.onchange = function() {
   const classValue = this.value;
