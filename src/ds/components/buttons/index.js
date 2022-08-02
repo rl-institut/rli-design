88af3412bbe16894a528221187422134021fae1b
Bryan Lancien <bryanlancien@yahoo.fr>
2022-08-02 13:42:17 +0100

    restructure buttons.js

--- src/ds/components/buttons/index.js
+++ src/ds/components/buttons/index.js
@@ -1,16 +1,27 @@
-const dashboardButton = document.getElementById('jsButton');
-const dashboardButtonText = dashboardButton.innerHTML;
-const dashboardButtonChild = dashboardButton.firstElementChild;
-const selectClass = document.getElementById('js-select-class');
+const bgColor = document.getElementById('js-bg-color');
+const button = document.getElementById('jsButton');
+const buttonText = button.innerHTML;
+const buttonChild = button.firstElementChild;
+const selectBg = document.getElementById('js-select-bg');
+const selectType = document.getElementById('js-select-type');
 const buttonCode = document.getElementById('js-button-code');
 const buttonCSSClass = document.getElementById('js-button-css-class');
-let selectedOption = selectClass.value;
+let selectedBg = selectBg.value;
+let selectedType = selectType.value;
 
-buttonCode.append(dashboardButtonText);
+buttonCode.append(buttonText);
 
-selectClass.onchange = function() {
-  const classValue = this.value;
-  dashboardButtonChild.classList.replace(selectedOption, classValue);
+const updateSnippet = function(classValue) {
   buttonCSSClass.innerHTML = `<code>.${classValue}</code>`;
-  selectedOption = classValue;
+}
+
+const updateDemo = function(e, selected, classValue) {
+  e.classList.replace(selected, classValue);
+}
+
+selectType.onchange = function() {
+  const classValue = this.value;
+  updateSnippet(classValue);
+  updateDemo(buttonChild, selectedType, classValue);
+  selectedType = classValue;
 }
