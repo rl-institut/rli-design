6b7849be47184976670681a8b20d815c6c70fc05
Bryan Lancien <bryanlancien@yahoo.fr>
2022-09-29 19:27:15 +0100

    finetune first chart style

--- src/ds/components/charts/index.js
+++ src/ds/components/charts/index.js
@@ -1,25 +1,36 @@
-const chartStyles = {
+const chartVariables = {
   colors: {
+    primary: '#002C50',
     grey: '#C3D1DC'
   },
   typography: {
     fontFamily: 'Roboto',
     fontSize: 14,
+    fontSizeSmall: 12,
+    fontWeight: 300,
+    fontColor: '#002C50',
     lineHeight: 20
   }
 };
 
 var dpSingleBar = echarts.init(document.getElementById('dp-single-bar'));
 
-const option = {
+const optionSingleBar = {
   title: {},
   legend: {},
-  tooltip: {},
+  tooltip: {
+    textStyle: {
+      color: chartVariables.typography.fontColor
+    }
+  },
   grid: {
     left: '30%',
     right: '30%',
     bottom: 20,
     top: 30
+  },
+  textStyle: {
+    color: chartVariables.typography.fontColor,
   },
   dataset: {
     source: [
@@ -27,23 +38,40 @@ const option = {
       ['2021', 943.231]
     ]
   },
-  xAxis: { type: 'category' },
+  xAxis: { 
+    type: 'category',
+    axisLabel: {
+      fontSize: chartVariables.typography.fontSizeSmall,
+      fontWeight: chartVariables.typography.fontWeight
+    },
+    axisLine: {
+      lineStyle: {
+        color: chartVariables.colors.primary
+      }
+    }
+  },
   yAxis: {
+    type: 'value',
+    axisLabel: {
+      fontSize: chartVariables.typography.fontSizeSmall,
+      fontWeight: chartVariables.typography.fontWeight
+    },
     name: 'EinwohnerInnen (Tsd.)',
     nameTextStyle: {
-      align: 'left'
-    },
-    type: 'value',
+      align: 'left',
+      fontSize: chartVariables.typography.fontSize,
+      fontWeight: chartVariables.typography.fontWeight
+    }
   },
   series: [
     { 
       type: 'bar',
       color: [
-        chartStyles.colors.grey
+        chartVariables.colors.grey
       ],
       barWidth: 40
     }
   ]
 };
 
-dpSingleBar.setOption(option);
+dpSingleBar.setOption(optionSingleBar);
