ee22fa4029ab9d1d579b6922fa15c3aa1d3e84ec
Bryan Lancien <bryanlancien@yahoo.fr>
2022-09-30 10:25:40 +0100

    change from dataset to series.data

--- src/ds/components/charts/index.js
+++ src/ds/components/charts/index.js
@@ -24,10 +24,9 @@ const chartVariables = {
 };
 
 var dpSingleBar = echarts.init(document.getElementById('dp-single-bar'));
+var dpTwoBars = echarts.init(document.getElementById('dp-two-bars'));
 
 const optionSingleBar = {
-  title: {},
-  legend: {},
   tooltip: {
     textStyle: {
       color: chartVariables.typography.fontColor
@@ -42,14 +41,11 @@ const optionSingleBar = {
   textStyle: {
     color: chartVariables.typography.fontColor,
   },
-  dataset: {
-    source: [
-      ['EinwohnerInnen (Tsd.)'],
-      ['2021', 943.231]
-    ]
-  },
   xAxis: { 
     type: 'category',
+    data: [
+      '2021'
+    ],
     axisLabel: {
       fontSize: chartVariables.typography.fontSizeSmall,
       fontWeight: chartVariables.typography.fontWeight
@@ -76,6 +72,73 @@ const optionSingleBar = {
   series: [
     { 
       type: 'bar',
+      data: [
+        943.231
+      ],
+      color: [
+        chartVariables.colors.grey
+      ],
+      barWidth: chartVariables.bars.singleWidth
+    }
+  ]
+};
+
+const optionTwoBars = {
+  tooltip: {
+    textStyle: {
+      color: chartVariables.typography.fontColor
+    }
+  },
+  grid: {
+    left: chartVariables.grid.left,
+    right: chartVariables.grid.right,
+    bottom: chartVariables.grid.bottom,
+    top: chartVariables.grid.top
+  },
+  textStyle: {
+    color: chartVariables.typography.fontColor,
+  },
+  xAxis: { 
+    type: 'category',
+    data: [
+      '2021', 
+      '2035'
+    ],
+    axisLabel: {
+      fontSize: chartVariables.typography.fontSizeSmall,
+      fontWeight: chartVariables.typography.fontWeight
+    },
+    axisLine: {
+      lineStyle: {
+        color: chartVariables.colors.primary
+      }
+    }
+  },
+  yAxis: {
+    type: 'value',
+    axisLabel: {
+      fontSize: chartVariables.typography.fontSizeSmall,
+      fontWeight: chartVariables.typography.fontWeight
+    },
+    name: 'Stromerzeugung (GWh)',
+    nameTextStyle: {
+      align: 'left',
+      fontSize: chartVariables.typography.fontSize,
+      fontWeight: chartVariables.typography.fontWeight
+    }
+  },
+  series: [
+    {
+      type: 'bar',
+      data: [
+        45,
+        {
+          value: 82,
+          itemStyle: {
+            color: chartVariables.colors.primary
+          }
+        }
+      ],
       color: [
         chartVariables.colors.grey
       ],
@@ -85,3 +148,4 @@ const optionSingleBar = {
 };
 
 dpSingleBar.setOption(optionSingleBar);
+dpTwoBars.setOption(optionTwoBars);
