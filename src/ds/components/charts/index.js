cd04b914752853a76357e866bce67ce7119c0630
Bryan Lancien <bryanlancien@yahoo.fr>
2022-10-03 13:44:58 +0100

    add charts with patterns

--- src/ds/components/charts/index.js
+++ src/ds/components/charts/index.js
@@ -4,9 +4,11 @@ const chartVariables = {
   colors: {
     primary: '#002C50',
     grey: '#C3D1DC',
+    greenLighter: '#97F0D9',
     green: '#06DFA7',
-    orange: '#E6A100',
     greenDarker: '#00BC8C',
+    orangeLighter: '#F0D187',
+    orange: '#E6A100',
     orangeDarker: '#BE880B'
   },
   typography: {
@@ -79,6 +81,8 @@ var dpSingleBar = echarts.init(document.getElementById('dp-single-bar'));
 var dpTwoBars = echarts.init(document.getElementById('dp-two-bars'));
 var dpComparisonBars1 = echarts.init(document.getElementById('dp-comparison-bars1'));
 var dpComparisonBars2 = echarts.init(document.getElementById('dp-comparison-bars2'));
+var dpComparisonBarsPattern1 = echarts.init(document.getElementById('dp-comparison-bars-pattern1'));
+var dpComparisonBarsPattern2 = echarts.init(document.getElementById('dp-comparison-bars-pattern2'));
 
 // Bar chart with single column
 const optionSingleBar = {
@@ -256,10 +260,145 @@ const optionTwoBarsComparison2 = {
         {
           value: 30,
           itemStyle: {
-            color: chartVariables.colors.orange,
+            color: chartVariables.colors.orange
+          },
+        }
+      ],
+      color: [
+        chartVariables.colors.grey
+      ],
+      barWidth: chartVariables.bars.smallWidth,
+      markLine: {
+        data: [
+          { 
+            yAxis: 19,
+            lineStyle: {
+              color: chartVariables.colors.orangeDarker,
+              width: chartVariables.markLine.width,
+              type: 'solid'
+            }
+          }
+        ],
+        tooltip: {
+          show: false
+        },
+        symbol:['none', 'none']
+      }
+    }
+  ]
+};
+
+// 2 bar charts with two columns for comparison and patterns
+const optionTwoBarsComparisonPattern1 = {
+  title: {
+    text: 'Anteil Erneuerbare \nEnergien (%)',
+    textStyle: values.titleStyle
+  },
+  tooltip: values.tooltip,
+  grid: chartVariables.gridsm,
+  textStyle: values.textStyle,
+  xAxis: { 
+    type: 'category',
+    data: [
+      '2021', 
+      '2035'
+    ],
+    axisLabel: values.axisLabel,
+    axisLine: values.axisLine,
+    axisTick: {
+      show: chartVariables.ticks.show
+    }
+  },
+  yAxis: {
+    type: 'value',
+    axisLabel: values.axisLabel
+  },
+  series: [
+    {
+      type: 'bar',
+      data: [
+        45,
+        {
+          value: 93,
+          itemStyle: {
+            color: 'rgba(0, 0, 0, 0)',
             decal: {
               show: true,
-              symbol: 'triangle'
+              symbol: 'rect',
+              color: chartVariables.colors.greenLighter,
+              dashArrayX: [1, 0],
+              dashArrayY: [2, 5],
+              symbolSize: 1,
+              rotation: Math.PI / 6
+            }
+          },
+        }
+      ],
+      color: [
+        chartVariables.colors.grey
+      ],
+      barWidth: chartVariables.bars.smallWidth,
+      markLine: {
+        data: [
+          { 
+            yAxis: 93,
+            lineStyle: {
+              color: chartVariables.colors.greenDarker,
+              width: chartVariables.markLine.width,
+              type: 'solid'
+            }
+          }
+        ],
+        tooltip: {
+          show: false
+        },
+        symbol:['none', 'none']
+      }
+    }
+  ]
+};
+
+const optionTwoBarsComparisonPattern2 = {
+  title: {
+    text: 'CO2-Emissionen (Mt)',
+    textStyle: values.titleStyle
+  },
+  tooltip: values.tooltip,
+  grid: chartVariables.gridsm,
+  textStyle: values.textStyle,
+  xAxis: { 
+    type: 'category',
+    data: [
+      '2021', 
+      '2035'
+    ],
+    axisLabel: values.axisLabel,
+    axisLine: values.axisLine,
+    axisTick: {
+      show: chartVariables.ticks.show
+    }
+  },
+  yAxis: {
+    type: 'value',
+    axisLabel: values.axisLabel
+  },
+  series: [
+    {
+      type: 'bar',
+      data: [
+        70,
+        {
+          value: 19,
+          itemStyle: {
+            color: 'rgba(0, 0, 0, 0)',
+            decal: {
+              show: true,
+              symbol: 'rect',
+              color: chartVariables.colors.orangeLighter,
+              dashArrayX: [1, 0],
+              dashArrayY: [2, 5],
+              symbolSize: 1,
+              rotation: Math.PI / 6
             }
           },
         }
@@ -292,3 +431,5 @@ dpSingleBar.setOption(optionSingleBar);
 dpTwoBars.setOption(optionTwoBars);
 dpComparisonBars1.setOption(optionTwoBarsComparison1);
 dpComparisonBars2.setOption(optionTwoBarsComparison2);
+dpComparisonBarsPattern1.setOption(optionTwoBarsComparisonPattern1);
+dpComparisonBarsPattern2.setOption(optionTwoBarsComparisonPattern2);
