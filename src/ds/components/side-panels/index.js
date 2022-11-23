5f7c285feadcbffacab8ca1a94938c47397248a4
Bryan Lancien <bryanlancien@yahoo.fr>
2022-11-23 15:16:13 +0000

    add dummy sidepanel from digiplan

--- src/ds/components/side-panels/index.js
+++ src/ds/components/side-panels/index.js
@@ -1,1 +1,4 @@
-// tbd
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
