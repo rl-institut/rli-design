be4cb8fe986570054981da9f268783359a2f6582
hedwiglieselotte <hedwig.bartels@rl-institut.de>
2023-02-14 17:05:58 +0100

    Organise charts part two.

--- src/charts/main.js
+++ src/charts/main.js
@@ -2,46 +2,6 @@ import { optionGradient, overview_option } from "./options";
 
 const overview = echarts.init(document.getElementById("overview"));
 
-const option = {
-  title: {
-    text: "Budget in Tsd. €",
-    textStyle: {
-      color: "#002C50",
-      fontWeight: "normal",
-      fontFamily: "Roboto",
-      fontSize: 14
-    }
-  },
-  tooltip: {
-    trigger: "axis",
-    axisPointer: {
-      type: "shadow"
-    }
-  },
-  dataset: {
-    source: [
-      ["Kategorie", "IST", "Forecast", "Abbau Überschüsse, Puffer und Risikobudget in Tsd. €"],
-      ["Personal", -12, -25, -65],
-      ["Eigenmittel", 72, 95, -65],
-    ]
-  },
-  xAxis: { type: "category" },
-  yAxis: {},
-  series: [{ type: "bar" }, { type: "bar" }, { "type": "line" }],
-  color: [
-    "#002C50",
-    "#FFE8B3"
-  ],
-  label: {
-    normal: {
-      position: "top",
-      distance: 10,
-      show: true,
-      formatter: ["Label Text"].join("\n")
-    }
-  }
-};
-
 function resizeChart() {
   setTimeout(function () {
       overview.resize();
