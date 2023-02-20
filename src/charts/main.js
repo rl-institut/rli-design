d910c8666de1e1b80ad0c7905da6e00432d3c8a8
hedwiglieselotte <hedwig.bartels@rl-institut.de>
2023-02-20 14:14:35 +0100

    Add overview generation chart.

--- src/charts/main.js
+++ src/charts/main.js
@@ -2,15 +2,18 @@ import { detailed_overview_option, optionGradient, overview_option } from "./opt
 
 const overview = echarts.init(document.getElementById("overview"));
 const detailed_overview = echarts.init(document.getElementById("detailed_overview"));
+const overview_generation = echarts.init(document.getElementById("overview_generation"));
 
 function resizeChart() {
   setTimeout(function () {
       overview.resize();
       detailed_overview.resize();
-  }, 300);
+      overview_generation.resize ();
+  }, 200);
 };
 
 overview.setOption(overview_option);
-overview.setOption(detailed_overview_option);
+detailed_overview.setOption(detailed_overview_option);
+overview_generation.setOption(overview_generation);
 
 window.addEventListener("resize", resizeChart); 
--- src/charts/options.js
+++ src/charts/options.js
@@ -147,3 +147,96 @@ export const overview_option = {
       },
     ]
   };
+  export const overview_generation_option = {
+    title: {
+        text: 'Übersicht Erzeugung'
+      },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        // Use axis to trigger tooltip
+        type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+      }
+    },
+    legend: {},
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: {
+      type: 'value'
+    },
+    yAxis: {
+      type: 'category',
+      data: ['Ziel Szenario', 'Mein Szenario', 'Status Quo']
+    },
+    series: [
+      {
+        name: 'Wind',
+        type: 'bar',
+        stack: 'total',
+        color: '#1F82C0',
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [502, 334, 230]
+      },
+      {
+        name: 'Bioenergie',
+        type: 'bar',
+        stack: 'total',
+        color: '#98D47E',
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [136, 134, 130]
+      },
+      {
+        name: 'Freiflächen - PV',
+        type: 'bar',
+        stack: 'total',
+        color: '#F6B93B',
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [382, 234, 130 ]
+      },
+      {
+        name: 'Aufdach - PV',
+        type: 'bar',
+        stack: 'total',
+        color: '#FFD660',
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [312, 254, 130]
+      },
+      {
+        name: 'Konventionell',
+        type: 'bar',
+        stack: 'total',
+        color: '#CFCFCF', 
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [132, 534, 1130 ]
+      },
+    ]
+  };
