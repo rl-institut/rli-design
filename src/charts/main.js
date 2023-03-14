669aa055f3018533e873d0cd8f572792374038b3
hedwiglieselotte <hedwig.bartels@rl-institut.de>
2023-03-14 16:57:12 +0100

    Add chart details for 'Strom' Overview

--- src/charts/main.js
+++ src/charts/main.js
@@ -4,7 +4,8 @@ import {
   overview_option,
   overview_generation_option,
   GHG_overview_option, 
-  overview_comparison_option
+  overview_comparison_option,
+  strom_overview,
 } from "./options";
 
 const overview = echarts.init(document.getElementById("overview"));
@@ -12,6 +13,7 @@ const detailed_overview = echarts.init(document.getElementById("detailed_overvie
 const overview_generation = echarts.init(document.getElementById("overview_generation"));
 const GHG_overview = echarts.init(document.getElementById("GHG_overview"));
 const overview_comparison = echarts.init(document.getElementById("overview_comparison"));
+const strom_overview = echarts.init(document.getElementById("strom_overview"));
 
 function resizeChart() {
   setTimeout(function () {
@@ -28,5 +30,6 @@ detailed_overview.setOption(detailed_overview_option);
 Overview_generation.setOption(overview_generation);
 GHG_overview.setOption(GHG_overview);
 overview_comparison.setOption(overview_comparison);
+strom_overview.setOption(strom_overview);
 
-window.addEventListener("resize", resizeChart); 
+window.addEventListener("resize", resizeChart);
