e899e28cb7cdf4d21a575224c99a1ff4aa749885
hedwiglieselotte <hedwig.bartels@rl-institut.de>
2023-05-04 14:04:41 +0200

    Add options_old for old charts.

--- src/charts/options_old.js
+++ src/charts/options_old.js
@@ -43,8 +43,7 @@ export const overview_option = {
     tooltip: {
       trigger: 'axis',
       axisPointer: {
-        // Use axis to trigger tooltip
-        type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+        type: 'shadow' 
       }
     },
     legend: {},
@@ -127,5 +126,65 @@ export const overview_option = {
         },
         data: [132, 534, 1130 ]
       },
+    ]
+  };
+  export const overview_comparison_option = {
+    title: {
+        text: 'Erzeugung - Verbrauch '
+      },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {
+      data: [ 'Erzeugung', 'Verbrauch']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    yAxis: [
+      {
+        type: 'category',
+        axisTick: {
+          show: false
+        },
+        data: ['Ziel', 'Szenario', 'Status Quo']
+      }
+    ],
+    series: [
+      {
+        name: 'Verbrauch',
+        type: 'bar',
+        stack: 'Total',
+        label: {
+          show: false
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [200, 230, 341]
+      },
+      {
+        name: 'Erzeugung',
+        type: 'bar',
+        stack: 'Total',
+        label: {
+          show: false,
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [-220, -200, -100 ]
+      }
     ]
   };
